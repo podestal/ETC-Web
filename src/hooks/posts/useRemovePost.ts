be995c142cbc671f949493d94d02fe2fa32bd0ab
podestal <luisrodriguezpodesta@gmail.com>
2024-06-27 10:35:42 -0500

    post CRUD sync api-client

--- src/hooks/posts/useRemovePost.ts
+++ src/hooks/posts/useRemovePost.ts
@@ -19,14 +19,19 @@
 
 // export default useUpdatePost
 
-import { useMutation, UseMutationResult } from "@tanstack/react-query"
+import { useMutation, UseMutationResult, useQueryClient } from "@tanstack/react-query"
 import getPostService, { Post, DeletePostData } from "../../services/api/postService"
+import { CACHE_KEY_POSTS } from "../../constants/queryKeys";
 
 const useDeletePost = (postId: number): UseMutationResult<Post, Error, DeletePostData> => {
+    const queryClient = useQueryClient()
     const postService = getPostService(postId)
     return useMutation({
         mutationFn: (data: DeletePostData) => postService.delete(data.access),
-        onSuccess: res => console.log(res),
+        onSuccess: res => {
+            console.log('response', res)
+            queryClient.setQueryData(CACHE_KEY_POSTS, ((posts: Post[]) => posts.filter(post => post.id !== postId)))
+        },
         onError: err => console.log(err)
     })
 }
--- src/hooks/posts/useUpdatePost.ts
+++ src/hooks/posts/useUpdatePost.ts
@@ -1,16 +1,22 @@
-import { useMutation, UseMutationResult } from "@tanstack/react-query";
+import { useMutation, UseMutationResult, useQueryClient } from "@tanstack/react-query";
 import getPostService, { PostData } from "../../services/api/postService";
 import { Post } from "../../services/api/postService";
+import { CACHE_KEY_POSTS } from "../../constants/queryKeys";
 
 const useUpdatePost = (postId: number): UseMutationResult<Post, Error, PostData> => {
-    // const queryClient = useQueryClient()
-    // const userId = 1
+    const queryClient = useQueryClient()
     const postService = getPostService(postId)
     return useMutation({
         mutationFn: (data: PostData) => postService.update(data.post, data.access),
         onSuccess: (savedPost, newPost) => {
             console.log('savedPost', savedPost)
             console.log('newPost', newPost);
+            queryClient.setQueryData(CACHE_KEY_POSTS, ((posts: Post[]) => posts.map(post => {
+                if (post.id == postId) {
+                    return {...post, ...savedPost}
+                }
+                return post
+            })))
         },
         onError: err => console.log(err)
         
